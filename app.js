f0e2bea6b73af9079ace5751ca01a28c47cef8ba
jackdgerrard <jackdgerrard@users.noreply.github.com>
2017-11-29 11:06:06 +0000

    working again

--- app.js
+++ app.js
@@ -1,60 +1,57 @@
 /*
-* EXPRESS CONSTANTS - for cleaner syntax
+ * EXPRESS CONSTANTS - for cleaner syntax
  */
-
 const express = require('express');
 const app = express();
 
 /*
-* DATABASE CONSTANTS - for cleaner syntax in render function further down
-*/
+ * DATABASE CONSTANTS - for cleaner syntax in render function further down
+ */
 
 const artists = require('./database').artists;
 const artwork = require('./database').artwork;
 
-
- /*
+/*
  * SET EXPRESS SETTINGS - port number, viewengine(jade/pug), static resources(css, javascript)
  */
-
-app.set('port', process.env.PORT || 3000 ); //CHANGE THIS IF 3000 IN USE
+app.set('port', process.env.PORT || 3000); //CHANGE THIS IF 3000 IN USE
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
 /*
-* INDEX - different ways to get to home page
-*/
+ * INDEX - different ways to get to home page
+ */
 app.get('/', function (req, res) {
-   res.render('index')
- })
-
- app.get('/index', function (req, res) {
   res.render('index')
 })
 
+app.get('/index', function (req, res) {
+  res.render('index')
+})
 
 /*
-* GET ALL ARTISTS - renders artists pug template and passes the artists from the database into it
-*/
-app.get('/artists',(req, res, next) => {
+ * GET ALL ARTISTS - renders artists pug template and passes the artists from the database into it
+ */
+app.get('/artists', (req, res, next) => {
   artists.all((err, artists) => {
-    if (err) return next (err);
-    res.render('artists.pug', {artists:artists}); // this is why we set the const further up
+    if (err) return next(err);
+    res.render('artists.pug', {
+      artists: artists
+    }); // this is why we set the const further up
   });
 });
 
 /*
-* GET ALL ARTWORK - renders artwork pug template and passes the artwork from the database into it
-*/
-app.get('/artwork',(req, res, next) => {
+ * GET ALL ARTWORK - renders artwork pug template and passes the artwork from the database into it
+ */
+app.get('/artwork', (req, res, next) => {
   artwork.all((err, artists) => {
-    if (err) return next (err);
-    res.render('artwork.pug', {artwork:artwork}); // this is why we set the const further up
+    if (err) return next(err);
+    res.render('artwork.pug', {
+      artwork: artwork
+    }); // this is why we set the const further up
   });
 });
-
-
-
 
 app.get('/basket', function (req, res) {
   res.render('basket')
@@ -64,8 +61,7 @@ app.get('/log-in', function (req, res) {
   res.render('log-in')
 })
 
-
 /*
-* LAUNCH APP - listen on port set further up, also has node/nodemon print the url to go to for testing/inspection
-*/
-app.listen(app.get('port'), () => console.log('go to http://localhost:'+app.get('port')))
+ * LAUNCH APP - listen on port set further up, also has node/nodemon print the url to go to for testing/inspection
+ */
+app.listen(app.get('port'), () => console.log('go to http://localhost:' + app.get('port')))
--- database.js
+++ database.js
@@ -3,67 +3,78 @@
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('database.db')
 
-
 /***********
-** ARTISTS class
-*////////////
+ ** ARTISTS class
+ ****************/
 
-class artists{
+class artists {
 
   static all(cb) { // get all artists
     db.all('SELECT * FROM ARTISTS', cb)
   }
 
-  static find(id, cb){ //get artists with certain ID
+  static find(id, cb) { //get artists with certain ID
     db.get('SELECT * FROM ARTISTS WHERE ID=?', id, cb)
   }
 
-  static create(id, cb){ // create a new artist with tite and content, change this to match
+  static create(id, cb) { // create a new artist with tite and content, change this to match
     const sql = 'INSERT INTO ARTISTS (title, content) VALUES(?,?)'
     db.run(sql, data.title, data.content, cb)
   }
 
-  static delete(id, cb){ // delete artist with ID passed in
+  static delete(id, cb) { // delete artist with ID passed in
     if (!id) return cb(new ERROR('please provide an ID'))
     db.run('delete from ARTISTS where id=?', id, cb)
   }
 
-  /***********
-  ** ARTWORKclass same functions as above
-  *////////////
-
 }
 
-class artwork{
+/*******************************************************************
+ ** ARTWORK class same functions as above
+ **********************************************************************/
 
-    static all(cb){
-      db.all('SELECT * FROM ARTWORK', cb)
-    }
-
-    static find(id, cb){
-      db.get('SELECT * FROM ARTWORK WHERE ID=?', id, cb)
-    }
-
-    static create(id, cb){
-      const sql = 'INSERT INTO ARTWORK (title, content) VALUES(?,?)'
-      db.run(sql, data.title, data.content, cb)
-    }
-
-    static delete(id, cb){
-      if (!id) return cb(new ERROR('please provide an ID'))
-      db.run('delete from ARTWORK where id=?', id, cb)
-    }
-
-
+class artwork {
+  static all(cb) {
+    db.all('SELECT * FROM ARTWORK', cb)
   }
 
+  static find(id, cb) {
+    db.get('SELECT * FROM ARTWORK WHERE ID=?', id, cb)
+  }
+
+  static create(id, cb) {
+    const sql = 'INSERT INTO ARTWORK (title, content) VALUES(?,?)'
+    db.run(sql, data.title, data.content, cb)
+  }
+
+  static delete(id, cb) {
+    if (!id) return cb(new ERROR('please provide an ID'))
+    db.run('delete from ARTWORK where id=?', id, cb)
+  }
+}
+
+
+class users {
+
+  static all(cb) {
+    db.all('SELECT * FROM users', cb)
+  }
+
+  static create(id, cb) {
+    const sql = 'INSERT INTO users (title, content) VALUES(?,?)'
+    db.run(sql, data.title, data.content, cb)
+  }
+}
+
+/******************
+ * EXPORTS
+ ****************/
+
+module.exports.artists = artists
+module.exports.artwork = artwork
+module.exports.users= users
+module.exports = db
+
+
 //close the database
-//db.close()
-
-/*
-* EXPORTS
-*/
-
-module.exports = db;
-module.exports.artists=artists;
-module.exports.artwork=artwork;
+db.close()
